d73bd3c06b9a5586519dea639ec77099b565a705
Lorenzo Alberton <quipo@php.net>
2004-03-25 14:33:58 +0000

    use quotes in define()
    git-svn-id: http://svn.php.net/repository/pear/packages/DB_QueryTool/trunk@154559 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/index.php
+++ tests/index.php
@@ -9,15 +9,15 @@ require_once 'DB/QueryTool.php';
 require_once 'PHPUnit.php';
 require_once 'PHPUnit/GUI/HTML.php';
 
-define(DB_DSN,'mysql://root@localhost/test');
-define(TABLE_USER,      'QueryTool_user');
-define(TABLE_ADDRESS,   'QueryTool_address');
+define('DB_DSN', 'mysql://root@localhost/test');
+define('TABLE_USER',      'QueryTool_user');
+define('TABLE_ADDRESS',   'QueryTool_address');
 
-define(TABLE_QUESTION,  'question');
-define(TABLE_ANSWER,    'answer');
+define('TABLE_QUESTION',  'question');
+define('TABLE_ANSWER',    'answer');
 
 $allTables = array(TABLE_USER,TABLE_ADDRESS,TABLE_QUESTION,TABLE_ANSWER);
-require_once 'sql.php'; 
+require_once 'sql.php';
 
 require_once 'UnitTest.php';
 require_once 'Common.php';
