45f7a64373076dd0073a990bf4ce15d347457274
Lorenzo Alberton <quipo@php.net>
2004-04-27 22:35:12 +0000

    - add testcase for empty result - CS
    git-svn-id: http://svn.php.net/repository/pear/packages/DB_QueryTool/trunk@157313 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/GetAll.php
+++ tests/GetAll.php
@@ -8,10 +8,10 @@ class tests_GetAll extends tests_UnitTest
     function _setup()
     {
         $this->user =& new tests_Common(TABLE_USER);
-        $this->user->add(array('name'=>'some name'));
-        $this->user->add(array('name'=>'some name1'));
-        $this->user->add(array('name'=>'some name2'));
-        $this->user->add(array('name'=>'some name3'));
+        $this->user->add(array('name' => 'some name'));
+        $this->user->add(array('name' => 'some name1'));
+        $this->user->add(array('name' => 'some name2'));
+        $this->user->add(array('name' => 'some name3'));
     }
 
     function test_getAll()
@@ -19,35 +19,35 @@ class tests_GetAll extends tests_UnitTest
         $this->_setup();
         $this->assertEquals(4,sizeof($this->user->getAll()));
     }
-            
+
     function test_getAllWhereSearch()
     {
         $this->_setup();
-        $this->user->addWhereSearch('name','some');
+        $this->user->addWhereSearch('name', 'some');
         $this->assertEquals(4,sizeof($this->user->getAll()));
     }
-    
+
     function test_getAllWhereSearch1()
     {
         $this->_setup();
-        $this->user->addWhereSearch('name','4');
+        $this->user->addWhereSearch('name', '4');
         $this->assertEquals(0,sizeof($this->user->getAll()));
     }
-            
+
     function test_getAllWhereSearch2()
     {
         $this->_setup();
-        $this->user->addWhereSearch('name','some name');
+        $this->user->addWhereSearch('name', 'some name');
         $this->assertEquals(2,sizeof($this->user->getAll(0,2)));
     }
-    
+
     function test_getAllWhereSearch8()
     {
         $this->_setup();
         $this->_setup();
         $this->assertEquals(8,sizeof($this->user->getAll()));
     }
-    
+
     function test_getAllWhereSearch10()
     {
         $this->_setup();
@@ -60,6 +60,13 @@ class tests_GetAll extends tests_UnitTest
         $this->_setup();
         $this->_setup();
         $this->assertEquals(8,sizeof($this->user->getAll(0,0)));
+    }
+
+    function test_getAllWhereSearchEmpty()
+    {
+        $this->_setup();
+        $this->user->addWhereSearch('name', 'some other name');
+        $this->assertFalse($this->user->getAll());
     }
 }
 
